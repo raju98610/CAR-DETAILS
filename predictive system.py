5e185450bf35b74f7acb0a010cb46c04f0bac4a5
Rajendra Prasad Ghosh <135945412+raju98610@users.noreply.github.com>
2023-07-20 19:56:49 +0530

    Add files via upload

---
+++ predictive system.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 19 20:39:03 2023
+
+@author: RAJU
+"""
+
+import numpy as np
+import pickle
+
+# Load the trained model
+loaded_model=pickle.load(open('C:/Users/RAJU/OneDrive/Desktop/MACHINE LEARNING PROJECT CAPSTONE/trained_model.sav','rb'))
